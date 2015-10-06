d67d286ba13d93ab2191f7e0d0459c6b67234094
Alec Tunbridge <alec.tunbridge@gmail.com>
2015-10-06 10:19:48 +0100

    Using flyway to setup derby database on start

--- src/main/java/hello/Configuration.java
+++ src/main/java/hello/Configuration.java
@@ -1,6 +1,7 @@
 package hello;
 
-import org.springframework.core.io.ClassPathResource;
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
 import org.springframework.integration.aop.PublisherAnnotationBeanPostProcessor;
 import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.handler.advice.RequestHandlerRetryAdvice;
@@ -8,13 +9,6 @@ import org.springframework.integration.jdbc.store.JdbcChannelMessageStore;
 import org.springframework.integration.jdbc.store.channel.DerbyChannelMessageStoreQueryProvider;
 import org.springframework.integration.store.MessageGroupQueue;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.context.annotation.*;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.DatabasePopulator;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.retry.RetryPolicy;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
@@ -35,38 +29,24 @@ public class Configuration {
 
     @Bean
     public DataSource dataSource() {
-//        EmbeddedDatabaseBuilder embeddedDatabaseBuilder = new EmbeddedDatabaseBuilder();
-//        return embeddedDatabaseBuilder
-//                .setType(EmbeddedDatabaseType.DERBY)
-//                .addScript("classpath:org/springframework/integration/jdbc/store/channel/schema-derby.sql")
-//                .setType()
-//                .build();
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.apache.derby.jdbc.EmbeddedDriver");
-        dataSource.setUrl("jdbc:derby:directory:alec;");
-        //TODO this needs to be conditional in some way
-        //DatabasePopulatorUtils.execute(databasePopulator(), dataSource);
+        dataSource.setUrl("jdbc:derby:directory:alec;create=true");
         return dataSource;
     }
 
-//    @Bean
-//    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
-//        final DataSourceInitializer initializer = new DataSourceInitializer();
-//        initializer.setDataSource(dataSource);
-//        initializer.setDatabasePopulator(databasePopulator());
-//        return initializer;
-//    }
-
-    private DatabasePopulator databasePopulator() {
-        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScript(new ClassPathResource("org/springframework/integration/jdbc/store/channel/schema-derby.sql"));
-        return populator;
+    @Bean(initMethod = "migrate")
+    Flyway flyway() {
+        Flyway flyway = new Flyway();
+        flyway.setBaselineOnMigrate(true);
+        flyway.setDataSource(dataSource());
+        return flyway;
     }
-
 
     @Bean
     public MessageChannel notificationQueue() {
-        QueueChannel queueChannel = new QueueChannel(new MessageGroupQueue(jdbcChannelMessageStore(),"notificationStore"));
+        QueueChannel queueChannel = new QueueChannel(
+                new MessageGroupQueue(jdbcChannelMessageStore(), "notificationStore"));
         return queueChannel;
     }
 
