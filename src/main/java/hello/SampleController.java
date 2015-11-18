f28de517e6d2cd3e08cc529c626f4612795ba4c8
Alec Tunbridge <alec.tunbridge@gmail.com>
2015-11-18 15:29:08 +0000

    Actually made the sql fetch generic...

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,6 +1,7 @@
 package hello;
 
 import org.jooq.DSLContext;
+import org.jooq.impl.TableImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,11 +11,14 @@ import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -104,7 +108,7 @@ public class SampleController implements ApplicationListener<ContextClosedEvent>
         }
     }
 
-    @RequestMapping("/db2hal/{clazz}")
+    @RequestMapping(value = "/db2hal/{clazz}")
     @ResponseBody
     public ResponseEntity<Resources> getJson(@PathVariable String clazz) {
         try {
@@ -116,8 +120,36 @@ public class SampleController implements ApplicationListener<ContextClosedEvent>
         return null;
     }
 
+    @RequestMapping(value = "/db2hal/{clazz}", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<Resource> postJson(@PathVariable String clazz, @RequestBody Resource resource) {
+        try {
+            Class entityClass = forName("org.jooq.example.gradle.db.app.tables.pojos." + clazz);
+            return new ResponseEntity<>(hal2db(entityClass), HttpStatus.OK);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Resource hal2db(Class entityClass) {
+        //connection.newRecord(DERIVATIVE, derivative).store();
+        return null;
+    }
+
     private Resources<Resource> db2hal(Class clazz) {
-        return new Resources<>((List<Resource>) connection.select().from(INT_CHANNEL_MESSAGE).fetchInto(clazz),linkTo(methodOn(SampleController.class).getJson(clazz.getSimpleName())).withSelfRel());
+        try {
+            return new Resources<>((List<Resource>) connection.select().from((TableImpl)
+                    forName("org.jooq.example.gradle.db.app.tables." + clazz.getSimpleName()).newInstance()
+            ).fetchInto(clazz),linkTo(methodOn(SampleController.class).getJson(clazz.getSimpleName())).withSelfRel());
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
